c0423d8a5c357f18c37202a762cf9820ece15a3d
marcamichelle1209-arch <marcamichelle1209@gmail.com>
2026-07-18 07:36:16 -0500

    inicio del proyecto

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        JFrame frmiVentana = new JFrame("Proyecto");
+        JPanel plnmiPanel = new JPanel();
+
+        frmiVentana.add(plnmiPanel);
+        frmiVentana.setSize(600,600);
+        frmiVentana.setVisible(true);
     }
 }
+
+// nicolnataly117@gmail.com mayancelaevelyn8@gmail.com
